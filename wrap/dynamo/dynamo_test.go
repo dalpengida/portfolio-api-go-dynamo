10bab15b078f14006bf3b681b97e6b0bb418cb2d
dalpengida <dalpengida@gmail.com>
2024-04-07 08:36:07 +0900

    - 폴더 정리
      - aws 기능 wrapping 한 코드들 wrap package 안으로 이동

---
+++ wrap/dynamo/dynamo_test.go
@@ -0,0 +1,165 @@
+package dynamo
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/dalpengida/portfolio-go-aws/common"
+	"github.com/dalpengida/portfolio-go-aws/model"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	TEST_TABLE_NAME        = "portfolio-test"
+	TEST_SUCESS_MSG_FORMAT = "[%s] success"
+)
+
+// Test_ListTable 는 테이블 리스트 조회 기능 테스트
+func Test_ListTable(t *testing.T) {
+	dynamoClient := New(TEST_TABLE_NAME)
+	tables, err := dynamoClient.ListTables(context.TODO())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Interface("tables", tables).Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
+
+// Test_CreateTable 는 테이블 생성 확인
+func Test_CreateTable(t *testing.T) {
+	dynamoClient := New(TEST_TABLE_NAME)
+	tableDesc, err := dynamoClient.CreateTable(context.TODO(), CREATE_TABLE_SCHEMA)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Interface("table_desc", tableDesc).Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
+
+// PutItem 는 아이템을 dynamo 에 upsert
+func Test_PutItem(t *testing.T) {
+	item := model.TestItem{
+		PK:      "pk",
+		SK:      "sk",
+		Val:     "val",
+		Updated: time.Now().Unix(),
+	}
+
+	dynamoClient := New(TEST_TABLE_NAME)
+	err := dynamoClient.PutItem(context.TODO(), item)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
+
+// Test_FindWithPK 는 pk 를 가지고 검색 기능 검사
+func Test_FindWithPK(t *testing.T) {
+	dynamoClient := New(TEST_TABLE_NAME)
+
+	pk := "pk"
+	var sliceObj []model.TestItem
+	err := dynamoClient.FindWithPK(context.TODO(), pk, &sliceObj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Interface("find_obj", sliceObj).Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
+
+// Test_FindBeginsWith 는 pk, prefixSK를 이용하여 데이터를 조회 하는 기능 검사
+func Test_FindBeginsWith(t *testing.T) {
+	dynamoClient := New(TEST_TABLE_NAME)
+
+	pk := "pk"
+	prefixSk := "sk"
+	var sliceObj []model.TestItem
+	err := dynamoClient.FindBeginsWith(context.TODO(), pk, prefixSk, &sliceObj, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Interface("find_items", sliceObj).Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
+
+// Test_MustFindOne 는 하나의 데이터가 있을 꺼라고 믿고 조회를 시도
+func Test_MustFindOne(t *testing.T) {
+	dynamoClient := New(TEST_TABLE_NAME)
+
+	pk := "pk"
+	sk := "sk"
+
+	var obj model.TestItem
+	err := dynamoClient.MustFindOne(context.TODO(), pk, sk, &obj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Interface("item", obj).Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+
+}
+
+// Test_DeleteItem 는 pk, sk 를 이용하여 item 삭제
+func Test_DeleteItem(t *testing.T) {
+	dynamoClient := New(TEST_TABLE_NAME)
+
+	pk := "pk"
+	sk := "sk"
+
+	err := dynamoClient.DeleteItem(context.TODO(), pk, sk)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
+
+// Test_BulkPutItems 는 한번에 여러건 넣을 수 있는 기능 검사
+func Test_BulkPutItems(t *testing.T) {
+	items := make([]model.TestItem, 0)
+	for i := 0; i < 30; i++ {
+		items = append(items, model.TestItem{
+			PK: "pk",
+			SK: fmt.Sprintf("bulksk#%d", i),
+		})
+	}
+
+	dynamoClient := New(TEST_TABLE_NAME)
+	err := dynamoClient.PutItemsWithBatch(context.TODO(), items)
+	if err != nil {
+		log.Error().Err(err).Msg("failed") // 일부러 실패를 한번 함, 초과 했을 경우를 확인 하기 위함
+	}
+
+	err = dynamoClient.PutItemsWithBatch(context.TODO(), items[:MAX_COUNT_BULK_ITME])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
+
+// Test_PutItemsWithTx 는 트랜잭션을 걸고 여러 item 을 넣을 경우 기능 검사
+func Test_PutItemsWithTx(t *testing.T) {
+	items := make([]model.TestItem, 0)
+	for i := 0; i < 101; i++ {
+		items = append(items, model.TestItem{
+			PK: "pk",
+			SK: fmt.Sprintf("bulksk#%d", i),
+		})
+	}
+
+	dynamoClient := New(TEST_TABLE_NAME)
+	err := dynamoClient.PutItemsWithTransaction(context.TODO(), items)
+	if err != nil {
+		log.Error().Err(err).Msg("failed") // 일부러 실패를 한번 함, 초과 했을 경우를 확인 하기 위함
+	}
+	err = dynamoClient.PutItemsWithTransaction(context.TODO(), items[:MAX_COUNT_TRANSACTION_ITEM])
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Debug().Msgf(TEST_SUCESS_MSG_FORMAT, common.FunctionName())
+}
---
+++ wrap/sqs/sqs.go
@@ -0,0 +1,173 @@
+package sqs
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/dalpengida/portfolio-go-aws/common"
+	"github.com/dalpengida/portfolio-go-aws/config"
+
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	client *sqs.Client
+
+	// sqs 의 경우 fifo 기능을 쓰기 위해서는 필수로 이름 끝에 fifo 가 붙어야 함
+	FIFO_QUEUE_SUFFIX = ".fifo"
+)
+
+type Queue struct {
+	QueueName string
+	queueUrl  *string
+}
+
+func init() {
+	client = sqs.NewFromConfig(config.GetAws())
+}
+
+func New(queueName string) Queue {
+	return Queue{
+		QueueName: queueName,
+	}
+}
+
+// Create queue 생성
+func (q Queue) Create(c context.Context, schema *sqs.CreateQueueInput) error {
+	if schema == nil {
+		schema = CREATE_SQS_SCHEMA
+	}
+	r, err := client.CreateQueue(c, schema)
+	if err != nil {
+		return fmt.Errorf("create queue faild, %w", err)
+	}
+
+	log.Debug().Interface("response", r).Msg("create queue success")
+
+	return nil
+}
+
+// getUrl 는 지정한 큐의 url 정보를 조회하여 reciver 한테 저장을 해줌
+// 단순하게 조회만 하는 것이 아니라 저장도 해주기 때문에 차라리 setUrl 로 함수명 변경해야 하나 고민 됨
+func (q *Queue) getUrl(c context.Context) (string, error) {
+	r, err := client.GetQueueUrl(c, &sqs.GetQueueUrlInput{
+		QueueName: aws.String(q.QueueName),
+	})
+	if err != nil {
+		return "", fmt.Errorf("get url failed, queue : %s, %w", q.QueueName, err)
+	}
+
+	log.Debug().Interface("response", r).Msg("get queue url success")
+	q.queueUrl = r.QueueUrl
+
+	return *r.QueueUrl, nil
+}
+
+//	Send 는 queue 정보를 검사를 해서 url 이 없으면 url 조회해서 넣어주고, name 을 보고 fifo 면 fifo에 맞게 전송하고 일반이면 일반으로 보내주는 함수
+//
+// 내부를 확인을 해보면 전송 타입에 따른 정보가 있지만, 아직은 v2에서는 제대로 구현이 되어 있지 않은 것으로 보임
+// 그래서 data 구조체를 넘겨야 할 경우 json 으로 marshaling 해서 전달을 하고 받는 쪽에서 다시 unmarshaling 하는 것으로
+// // This member is required.
+// // DataType *string,BinaryListValues [][]byte,	BinaryValue []byte, StringListValues []string
+func (q *Queue) Send(c context.Context, obj interface{}) error {
+	if obj == nil {
+		return fmt.Errorf("invalid obj or obj is nil")
+	}
+
+	if q.queueUrl == nil {
+		_, err := q.getUrl(c)
+		if err != nil {
+			return err
+		}
+	}
+
+	json, err := json.Marshal(obj)
+	if err != nil {
+		return fmt.Errorf("queue message json marshaling faeild, %w", err)
+	}
+
+	if strings.Contains(q.QueueName, FIFO_QUEUE_SUFFIX) {
+		return q.sendQueueFifo(c, string(json))
+	} else {
+		return q.sendQueue(c, string(json))
+	}
+}
+
+// sendQueue 일반 queue 에 메시지를 전송을 해줌
+func (q *Queue) sendQueue(c context.Context, message string) error {
+	r, err := client.SendMessage(c, &sqs.SendMessageInput{
+		QueueUrl:    q.queueUrl,
+		MessageBody: aws.String(message), // message body 값의 length 가 0 이어도 오류가 남
+		//  DelaySeconds: 0, // 0: 즉시 노출, 이외: 시간 만큼 있다가 노출
+		//MessageDeduplicationId: aws.String(""), // FIFO 타입에서는 필수, 중복 방지를 위하여 사용되는 값
+		// MessageGroupId:         aws.String(""), // FIFO 타입에서는 필수
+	})
+	if err != nil {
+		return fmt.Errorf("queue send message faeild, %w", err)
+	}
+
+	log.Debug().Interface("response", r).Msg("queue send success")
+
+	return nil
+}
+
+// sendQueueFifo fifo queue에 맞게 메시지를 전송을 해줌
+func (q *Queue) sendQueueFifo(c context.Context, message string) error {
+	messageId := uuid.NewString()
+
+	r, err := client.SendMessage(c, &sqs.SendMessageInput{
+		QueueUrl:    q.queueUrl,
+		MessageBody: aws.String(message), // message body 값의 length 가 0 이어도 오류가 남
+		//  DelaySeconds: 0, // 0: 즉시 노출, 이외: 시간 만큼 있다가 노출
+		MessageDeduplicationId: aws.String(messageId), // FIFO 타입에서는 필수, 중복 방지를 위하여 사용되는 값
+		MessageGroupId:         aws.String(messageId), // FIFO 타입에서는 필수
+	})
+	if err != nil {
+		return fmt.Errorf("fifo queue send message faeild, %w", err)
+	}
+
+	log.Debug().Interface("response", r).Msg("fifo queue send success")
+
+	return nil
+}
+
+// BulkSend 한번에 여러 메시지를 전송을 요청을 하는 기능
+// 최대 10개 까지만 가능, 그래서 내부적으로 10개가 넘을 경우 10개만 보내고 나머지는 나중에 보내는 형식으로 함
+func (q *Queue) BulkSend(c context.Context, entries []types.SendMessageBatchRequestEntry) error {
+	const MAX_SQS_REQUEST_COUNT = 10
+
+	for {
+		requestCount := MAX_SQS_REQUEST_COUNT
+		entryCount := len(entries)
+		if entryCount == 0 {
+			break
+		}
+
+		var requestEnties []types.SendMessageBatchRequestEntry
+		if entryCount < MAX_SQS_REQUEST_COUNT {
+			requestCount = entryCount
+		}
+
+		//entries, requestEnties = common.SliceShift[types.SendMessageBatchRequestEntry](entries, requestCount)
+		entries, requestEnties = common.SliceShift[types.SendMessageBatchRequestEntry](entries, requestCount)
+		r, err := client.SendMessageBatch(c, &sqs.SendMessageBatchInput{
+			Entries:  requestEnties,
+			QueueUrl: q.queueUrl,
+		})
+		if err != nil {
+			fmt.Println(err)
+			//return err
+		}
+		if len(r.Failed) > 0 {
+			fmt.Printf("send sqs message batch failed, %v", r.Failed)
+		}
+	}
+
+	return nil
+}
